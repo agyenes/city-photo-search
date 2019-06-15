fa9cf5482d6f455365c5c04f68d7b842031f7fc8
agyenes <aron.gyenes@gmail.com>
2019-06-15 12:47:07 +0200

    add search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypeaheadComponent } from './typeahead/typeahead.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypeaheadComponent
   ],
   imports: [
     BrowserModule,
